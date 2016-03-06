ad783786f953bfee4d307356ae83994f46b4ccef
Nikolas Skoufis <n.skoufis@gmail.com>
2016-03-07 01:33:26 +1100

    Change self.dishes to self.dishesKnown
    For better clarity as to what this variable represents

--- src/Chef.py
+++ src/Chef.py
@@ -20,7 +20,7 @@ class Chef:
         Arguments:
             name (string): The name of the chef
             skill (int): The skill of the chef, from 0 to 10
-            dishes (list of strings): The names of dishes that the chef can make
+            dishesKnown (list of strings): The names of dishes that the chef can make
         """
         self.name = name
 
@@ -32,7 +32,7 @@ class Chef:
         if not isinstance(dishes, list):
             raise TypeError("Dishes must be of type list, got value of type %s" % str(type(dishes)))
         else:
-            self.dishes = dishes
+            self.dishesKnown = dishes
 
     def cookDish(dish):
         """
@@ -40,7 +40,7 @@ class Chef:
         The quality of the dish depends on the skill of the chef. We use a normal distribution centered around the
         skill of the chef with standard deviation 2
         """
-        if dish in self.dishes:
+        if dish in self.dishesKnown:
             # We want quality to be an integer, minimum of MIN_QUALITY, max of MAX_QUALITY
             quality = gauss(self.skill, QUALITY_SIGMA)
             if quality < MIN_QUALITY:
@@ -49,4 +49,4 @@ class Chef:
                 quality = MAX_QUALITY
             return Dish(name=dish, quality=quality)
         else:
-            raise UnkownDishException("This dish is not known by chef %s. Known dishes are: %s" % (self.name, str(self.dishes))
+            raise UnkownDishException("This dish is not known by chef %s. Known dishes are: %s" % (self.name, str(self.dishesKnown))
