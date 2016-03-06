9ba5f02cbcd58a1c80bc8d6bdf579c478229c4ef
Nikolas Skoufis <n.skoufis@gmail.com>
2016-03-07 01:36:41 +1100

    Add a Waiter class who can make dishes

---
+++ src/Waiter.py
@@ -0,0 +1,34 @@
+class Waiter:
+
+    # This is raised when none of the available chefs know the dish
+    class DishUnavailableException(Exception):
+        pass
+
+    def __init__(self, name):
+        """
+        A waiter at our restaurant
+
+        Arguments:
+            name (string): The name of the waiter
+        """
+        self.name = name
+
+    def placeOrder(self, dishName, chefs):
+        """
+        Try to place an order for a given dish, given some list of chefs. Return the dish to the customer
+
+        Arguments:
+            dishName (String): The name of the dish that has been ordered
+            chefs (list of Chef): The chefs available to take the order
+        """
+        order = None
+        # See if any of the chefs know the dish
+        for chef in chefs:
+            if dishName in chef.dishes:
+                order = chef.cookDish
+                break
+        if order is None:
+            # None of the chefs knew the dish
+            raise DishUnavailableException("Unfortunately none of the chefs know how to make %s" % dishName)
+        else:
+            return order
